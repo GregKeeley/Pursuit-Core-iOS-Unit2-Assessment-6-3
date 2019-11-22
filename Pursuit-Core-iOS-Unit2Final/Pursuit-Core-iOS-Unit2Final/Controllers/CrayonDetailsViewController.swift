de1068b30e00e03de5e246e20d44eb22c1b46426
Gregory Keeley <gregorykeeley@pursuit.org>
2019-11-22 11:07:10 -0500

    - Created View controller for "CrayongDetailsViewController" with UI elements - Set constraints

--- Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/CrayonDetailsViewController.swift
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/CrayonDetailsViewController.swift
@@ -10,10 +10,14 @@ import UIKit
 
 class CrayonDetailsViewController: UIViewController {
 
+    @IBOutlet weak var titleBar: UINavigationItem!
+    
+    var crayon: Crayon?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-
+        view.backgroundColor = UIColor(red: CGFloat(crayon!.red), green: CGFloat(crayon!.green), blue: CGFloat(crayon!.blue), alpha: 0.50)
     }
     
 
--- Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/ViewController.swift
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/ViewController.swift
@@ -9,22 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-// reuse indentifier = "crayonCell"
+    
     
     @IBOutlet weak var tableView: UITableView!
     
     var crayons = [Crayon]()
     
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    tableView.dataSource = self
-  }
-
-
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        loadData()
+    }
+    func loadData() {
+        crayons = Crayon.allTheCrayons
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let CrayonDetailVC = segue.destination as? CrayonDetailsViewController,
+            let indexPath = tableView.indexPathForSelectedRow else {
+                fatalError("Failed to prepare for segue correctly")
+        }
+        let crayon = crayons[indexPath.row]
+        CrayonDetailVC.crayon = crayon
+        CrayonDetailVC.titleBar.title = crayon.name
+    }
+    
+    
 }
+
+
+
+//MARK: Extension
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "crayonCell", for: <#T##IndexPath#>)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "crayonCell", for: indexPath)
+        let crayon = crayons[indexPath.row]
+        cell.textLabel?.text = crayon.name
+        cell.detailTextLabel?.text = crayon.hex
+        cell.backgroundColor = UIColor(red: CGFloat(crayon.red), green: CGFloat(crayon.green), blue: CGFloat(crayon.blue), alpha: 0.50)
+        return cell
+        
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         crayons.count
